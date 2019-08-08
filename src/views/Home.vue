194e94fa4627f8bda886738073ebbee5211e2932
meeae <meeaeking@gmail.com>
2019-08-08 17:57:00 -0500

    add buttons to index page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>KidDo</h1>
+    <div>
+      Location: <input type="text" v-model="location">
+    </div>
+    <div>
+      <p><button v-on:click="getPlaydate('museums')">Museums</button></p>
+      <p><button v-on:click="getPlaydate('amusementparks')">Amusement Parks</button></p>
+      <p><button v-on:click="getPlaydate('beaches')">Beaches</button></p>
+      <p><button v-on:click="getPlaydate('parks')">Parks</button></p>
+      <p><button v-on:click="getPlaydate('playgrounds')">Playgrounds</button></p>
+      <p><button v-on:click="getPlaydate('arcades')">Arcades</button></p>
+    </div>
+    <div v-for="playdate in playdates">
+      <h2>{{ playdate.name }}</h2>
+      <h2>{{ playdate.location }}</h2>
+  
+    </div>
+    
+
   </div>
 </template>
 
+<style>
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      playdates: [],
+      location: ""
+    };
+  },
+  created: function() {
+    
+  },
+  methods: {
+    
+    getPlaydate: function(category) {
+
+      axios.get(`/api/playdates?location=${this.location}&categories=${category}`).then(response => {
+        this.playdates = response.data;
+      });
+    }
   }
-}
+};
 </script>
